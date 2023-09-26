712c550194bba54782060640311cd5f667881151
Dayo Farayola <dayojnr.3@gmail.com>
2023-09-26 11:54:20 +0100

    managing edge cases

--- components/TaskManager.js
+++ components/TaskManager.js
@@ -6,31 +6,39 @@ import {
   TextInput,
   TouchableOpacity,
   FlatList,
-  StyleSheet } from 'react-native';
+  StyleSheet,
+  Alert } from 'react-native';
 import Checkbox from 'expo-checkbox';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const TaskManager = () => {
+  //Get user's name from authentication context
   const { name } = useContext(AuthContext);
 
+  //Set state variables
   const [title, setTitle] = useState("");
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
-  const [editIndex, setEditIndex] = useState(-1);
+  const [editIndex, setEditIndex] = useState(-1); // Index of the task being edited (-1 means no task)
   const [isChecked, setChecked] = useState(tasks.map(() => false));
   const [count, setCount] = useState(0);
 
+  // Get navigation functionality for moving between screens
   const navigation = useNavigation();
 
   const handleAddTask = () => {
     if (task && title) {
       if (editIndex !== -1) {
-        // Edit existing task
-        const updatedTasks = [...tasks];
-        updatedTasks[editIndex] = { title, task };
-        setTasks(updatedTasks);
-        setEditIndex(-1);
+        // Edit existing task if an index is set
+        if (editIndex < tasks.length) {
+          const updatedTasks = [...tasks];
+          updatedTasks[editIndex] = { title, task };
+          setTasks(updatedTasks);
+          setEditIndex(-1); // Clear the edit index
+        } else {
+          console.log('Invalid edit index');
+        }
       } else {
         // Add new task
         setTasks([...tasks, { title, task }]);
@@ -38,27 +46,50 @@ const TaskManager = () => {
       }
       setTitle("");
       setTask("");
+    } else {
+      Alert.alert(
+        'Input Error!',
+        'Enter both Title and Task'
+      ),
+      Alert.prompt(
+        'Input Error!',
+        'Enter both Title and Task'
+      )
     }
   };
 
+  //Function to edit a task
   const handleEditTask = (index) => {
     const taskToEdit = tasks[index];
     setTitle(taskToEdit.title);
     setTask(taskToEdit.task);
     setEditIndex(index);
+    
   };
 
+  //Function to delete a task
   const handleDeleteTask = (index) => {
     const updatedTasks = [...tasks];
     updatedTasks.splice(index, 1);
     setTasks(updatedTasks);
-    setCount(count - 1);
+
+    const updateChecked = [...isChecked];
+    updateChecked[index] = !updateChecked[index];
+    
+    if (updateChecked[index] === false) {
+      setCount((prevState)=> prevState - 1 );
+    } else {
+      setCount((prevState)=> prevState + 0)
+    }
+    
   };
   
+  //Function to toggle the completion status of a task
   const handleToggleCheckbox = (index) => {
     const updateChecked = [...isChecked];
     updateChecked[index] = !updateChecked[index];
     setChecked(updateChecked);
+
     if (updateChecked[index] === false) {
       setCount((prevState)=> prevState + 1 );
     } else {
@@ -66,11 +97,13 @@ const TaskManager = () => {
     }
   };
 
+  //Function to handle logout/go back
   const handleLogout = () => {
-    // Then navigate back to the previous screen using navigation.goBack()
+    //Navigate back to the previous screen using navigation.goBack()
     navigation.goBack();
   };
 
+  //Function to render each task item
   const renderItem = ({ item, index }) => (
     <View style={styles.task}>
       <Checkbox
@@ -78,14 +111,22 @@ const TaskManager = () => {
         value={isChecked[index]}
         onValueChange={() => handleToggleCheckbox(index)}
       />
+
       <View style={[styles.tasks, { opacity: isChecked[index] ? 0.3 : 1 }]}>
         <Text style={styles.itemTitle}>{item.title}</Text>
         <Text style={styles.itemList}>{item.task}</Text>
       </View>
+
       <View style={styles.taskButtons}>
-        <TouchableOpacity onPress={() => handleEditTask(index)}>
-          <Text style={styles.editButton}>Edit</Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => handleEditTask(index)}
+        disabled={isChecked[index]} // Disable the "Edit" button when the checkbox is checked
+      >
+        <Text style={[styles.editButton, isChecked[index] && {opacity: 0.3}]}>
+          Edit
+        </Text>
+      </TouchableOpacity>
+
         <TouchableOpacity onPress={() => handleDeleteTask(index)}>
           <Text style={styles.deleteButton}>Delete</Text>
         </TouchableOpacity>
@@ -95,12 +136,14 @@ const TaskManager = () => {
 
   return (
     <SafeAreaView style={styles.container}>
+      {/* Display the user's name and a logout button */}
       <View style={styles.header}>
         <Text style={styles.title}>Welcome back <Text style={styles.username}>{name}</Text>!</Text>
         <TouchableOpacity onPress={handleLogout}>
           <Text style={styles.logoutButton}>Logout</Text>
-      </TouchableOpacity>
+        </TouchableOpacity>
       </View>
+
       <TextInput
         style={styles.input}
         placeholder="Enter title"
@@ -113,12 +156,20 @@ const TaskManager = () => {
         value={task}
         onChangeText={(text) => setTask(text)}
       />
-      <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
+
+      {/* Input fields for title and task */}
+      <TouchableOpacity 
+        style={[styles.addButton, { opacity: task && title ? 1 : 0.3 }]} 
+        onPress={handleAddTask}
+      >
         <Text style={styles.addButtonText}>
           {editIndex !== -1 ? 'Update Task' : 'Add Task'}
         </Text>
       </TouchableOpacity>
+
       <Text style={styles.countTask}>You have {count} task(s) left.</Text>
+
+      {/* Display the list of tasks */}
       <FlatList
         data={tasks}
         renderItem={renderItem}
